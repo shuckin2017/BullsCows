0a326c267f13a3bde81713dd426952532129687d
shuckin2017 <shuckin@mail.ru>
2020-11-15 02:24:10 +0300

    Шаблон сверстан / Авторизация [JSD-58]

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const loginPage = document.querySelector('.game-login'),
+      loginMessage = document.querySelector('.game-login__input'),
+      gamePage = document.querySelector('.game-page'),
+      userInfo = document.querySelector('.game-wrapper__user'),
+      startPage = document.querySelector('.game-start'),
+      loginButton = document.querySelector('.game-login__btn'),
+      startButton = document.querySelector('.before-block__btn-start');
+
+let arrLogins = [];
+
 const getNumber = () => {
   number = [];
 
@@ -10,4 +20,52 @@ const getNumber = () => {
   return number;
 };
 
-console.log(getNumber());
+const showUserInfo = () => {
+  const span = document.createElement('span'),
+        btnOut = document.createElement('button');
+
+  span.innerHTML = loginMessage.value;
+  span.className = 'user-info';
+  btnOut.className = 'btn game-wrapper__user-btn';
+  userInfo.appendChild(span);
+  userInfo.appendChild(btnOut);
+  return userInfo;
+};
+
+loginButton.addEventListener('click', () => {
+  if(loginMessage.value != ''){
+    pageHide(loginPage);
+    pageShow(gamePage);
+    arrLogins.push({login: loginMessage.value });
+    showUserInfo();
+    
+  } else {
+    alert('Введите логин');
+  }
+});
+
+startButton.addEventListener('click', () => {
+  pageHide(gamePage);
+  pageShow(startPage);
+});
+
+// btnOut.addEventListener('click', () => {
+//   pageHide(startPage);
+//   pageShow(gamePage);
+//   arrLogins.shift();
+// });
+
+function pageHide(element) {
+  setTimeout(() => {
+    element.setAttribute('hidden', '');
+  }, 1000);
+};
+
+function pageShow(element) {
+  setTimeout(() => {
+    element.removeAttribute('hidden');
+  }, 1000);
+};
+
+
+
